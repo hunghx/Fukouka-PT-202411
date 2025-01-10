eb1c02942b83ca991721327f8b55e92b1367cf54
hunghx <120539776+hunghx@users.noreply.github.com>
2025-01-10 21:29:12 +0700

    Add files via upload

--- project/js/data.js
+++ project/js/data.js
@@ -1,11 +1,11 @@
 // fix cứng dữ liệu mẫu
 let studentArray = [
     {studentId:"SV001",studentName:"Nguyễn Văn A", year:2000, address: "Phú Thọ", email:"anv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
-    {studentId:"SV002",studentName:"Nguyễn Văn B", year:2000, address: "Phú Thọ", email:"bnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
-    {studentId:"SV003",studentName:"Nguyễn Văn C", year:2000, address: "Phú Thọ", email:"cnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
-    {studentId:"SV004",studentName:"Nguyễn Văn D", year:2000, address: "Phú Thọ", email:"dnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
-    {studentId:"SV005",studentName:"Nguyễn Văn E", year:2000, address: "Phú Thọ", email:"env@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
-    {studentId:"SV006",studentName:"Nguyễn Văn F", year:2000, address: "Phú Thọ", email:"fnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"}
+    {studentId:"SV002",studentName:"Nguyễn Văn B", year:2004, address: "Phú Thọ", email:"bnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
+    {studentId:"SV003",studentName:"Nguyễn Văn C", year:2005, address: "Phú Thọ", email:"cnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
+    {studentId:"SV004",studentName:"Nguyễn Văn D", year:2002, address: "Phú Thọ", email:"dnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
+    {studentId:"SV005",studentName:"Nguyễn Văn E", year:2001, address: "Phú Thọ", email:"env@gmail.com", phone:"09873246543", sex:true, status:"Đang học"},
+    {studentId:"SV006",studentName:"Nguyễn Văn F", year:2007, address: "Phú Thọ", email:"fnv@gmail.com", phone:"09873246543", sex:true, status:"Đang học"}
 ]
 
 let classArray = [
--- project/js/student.js
+++ project/js/student.js
@@ -37,7 +37,7 @@ function showListStudent(students) {
     document.getElementById("tbody").innerHTML = html;
 }
 
-showListStudent(studentsList); // hiển thị lần đầu
+// showListStudent(studentsList); // hiển thị lần đầu
 
 
 // tạo hàm tiemf kiếm theo tên sinh viên
@@ -80,3 +80,58 @@ function sortStudent(property , by){
     // hiển thị lại danh sách sau khi sắp xếp
     showListStudent(studentsList);
 }
+
+// Phân trang : dùng để tối ưu các trang web cần hiển thị nhiều thông tin 
+// 1 trang nó bao gồm : số trang (vị trí của trang : 1), số phần tử trên 1 trang (kích thước : 5/10/15)
+
+// để lọc các phần tử theo số trang và kích thước :
+// Ví dụ : 100 sinh viên :
+// lấy ra các sinh viên ở trang thứ 1 , lấy 10 phần tử (0-> 9)
+// lấy ra các sinh viên ở trang thứ 2 , lấy 10 phần tử (10-> 19)
+// lấy ra các sinh viên ở trang thứ n , lấy 10 phần tử ((n-1)*10 -> n*10 - 1 )
+
+// hiển thị tổng số trang
+// sử dụng Math.ceil() để làm tròn lên khi tính tông số trang 
+
+
+function paginationStudentList(page, size){
+    console.log("page", page);
+    console.log("size", size);
+    
+    // lấy ra danh sách sinh viên thuộc trang đó 
+    let start = (page-1)*size;
+    let end = page*size;
+    let studentPage = studentsList.slice(start, end);
+    // if(end > studentsList.length-1){
+    //     end = studentsList.length-1;
+    // }
+    // for(let i = start; i<= end; i++ ){
+    //     studentPage.push(studentsList[i]);
+    // }
+//     <li class="page-item disabled">
+//     <a class="page-link">Previous</a>
+// </li>
+// <li class="page-item active"><a class="page-link" href="#">1</a></li>
+// <li class="page-item"><a class="page-link" href="#">2</a></li>
+// <li class="page-item"><a class="page-link" href="#">3</a></li>
+// <li class="page-item">
+//     <a class="page-link" href="#">Next</a>
+// </li>
+    let totalPage = Math.ceil(studentsList.length/size);
+    // hiển thi chân phần phân trang
+    let html = ` <li class="page-item ${page == 1?'disabled':''}">
+                    <a class="page-link" onclick="paginationStudentList(${page-1},${size})">Previous</a>
+                </li>`;
+    for(let i = 1; i<= totalPage; i++){
+        html += `<li class="page-item ${i == page?'active':''}"><a class="page-link" onclick="paginationStudentList(${i},${size})" href="#">${i}</a></li>`
+    }
+
+    html+=` <li class="page-item ${page == totalPage?'disabled':''}">
+                 <a class="page-link" href="#" onclick="paginationStudentList(${page+1},${size})">Next</a>
+            </li>`
+    document.getElementById("pagination").innerHTML = html;
+    showListStudent(studentPage);
+}
+
+// Phân trang lần đầu
+paginationStudentList(1,4);
